0388e173b5260e718aedab0c796e2a63077e978d
devnige <nigel.quinlan@myob.com>
2022-04-01 09:21:15 +1100

    add footer component

--- shopping-list-react/src/App.js
+++ shopping-list-react/src/App.js
@@ -5,25 +5,26 @@ import { Item } from './Item';
 import { shoppingList } from './Items';
 import { AddItem } from './AddItem';
 import { List } from './List';
+import { Footer } from './Footer';
 import './App.css';
 
 export default function App() {
   const [items, setItems] = useState(shoppingList);
-  const login = "MengpingBao";
+  const login = "elizabethholmes";
   return (
     <>
-      <GitHubUser login= { login } />
-      <Title name= { login } />
-      <AddItem items={ items } setItems={ setItems }/>
-      <List items={ items } renderItem={ (item) => {
-        return (
-            <Item key={item.id} name={item.name}>
-            </Item>
-          )
-      }} />
-      {/* <List items= {[1, 2, 3]} />
-      <List items= {["A", "string", "list"]} /> */}
-      
+      <div id="wrapper">
+        <GitHubUser login= { login } />
+        <Title name= { login[0].toUpperCase() + login.slice(1,) } />
+        <AddItem items={ items } setItems={ setItems }/>
+        <List items={ items } renderItem={ (item) => {
+            return (
+                <Item key={item.id} name={item.name}>
+                </Item>
+              )
+          }} />
+        <Footer year={new Date().getFullYear()} />
+      </div>
     </>
   );
 }
---
+++ shopping-list-react/src/Footer.js
@@ -0,0 +1,7 @@
+export function Footer(props) {
+    return (
+        <footer>
+            <p>Copyright FMA {props.year}</p>
+        </footer>
+    );
+}

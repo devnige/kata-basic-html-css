e5c2193cc22d730d1878c3379ff211a35c4fa0c6
devnige <nigel.quinlan@myob.com>
2022-11-24 16:47:44 +1100

    add a name variable

--- shopping-list-react/src/Title.test.js
+++ shopping-list-react/src/Title.test.js
@@ -3,7 +3,8 @@ import { render } from "@testing-library/react";
 import { Title } from "./Title";
 
 test("renders an h1", () => {
-    const { getByText } = render(<Title />);
-    const h1 = getByText(/My Shopping List/);
-    expect(h1).toHaveTextContent("My Shopping List");
+    const name = 'Josephinechong';
+    const { getByText } = render(<Title name={ name }/>);
+    const h1 = getByText(`${name}'s Shopping List`);
+    expect(h1).toHaveTextContent("Josephinechong's Shopping List");
 });
